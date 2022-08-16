53152b9326d0b0d1f5728f174b1eed6e60dc6ce1
Khushi Dayal <khushidayal25.94@gmail.com>
2022-08-16 22:27:29 +0800

    create model for user object

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const db = require('./../data/database');
+
+async function addUser(postData) {
+    try {
+        const result = await db.query('INSERT INTO users (name, email) VALUE(?);' , [
+            postData,
+        ]);
+        console.log('Inside addUser');
+        console.log(result);
+        return {id: result[0].insertId};
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getUser(id) {
+    try {
+        const query = `
+        SELECT * FROM users WHERE id = ? `;
+        const [users] = await db.query(query, [id]);
+        return users;
+    } catch(error) {
+        console.log(error);
+    }
+}
+async function updateUser(user,id) {
+    try {
+        const query = `
+        UPDATE users SET name = ? , email = ?
+        WHERE id = ?
+        `;
+        await db.query(query, [user.name, user.email, id]);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+async function deletUser(id) {
+    try {
+        await db.query('DELET FROM users WHERE id = ?', [id]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    addUser , getUser , updateUser , deletUser
+};
+
+    
