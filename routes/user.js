83f21d53b6ed1a6d57a42148e9ac7a9ef1964154
Khushi Dayal <khushidayal25.94@gmail.com>
2022-08-16 23:16:16 +0800

    Add endpoint to get and delete a user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
 const userController = require('./../controllers/userController');
 
 router.get('/healthcheck', userController.healthCheck);
 
 router.post('/users' , userController.addUser);
+
+router.get('/user/:id', userController.getUser);
+router.delete('/user/:id', userController.deleteUser);
 
 module.exports = router;
